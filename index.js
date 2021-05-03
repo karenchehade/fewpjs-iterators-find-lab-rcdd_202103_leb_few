5ba02912787365fb19358ae377ffac5559f12505
karen chehade <karen_chehade@hotmail.com>
2021-05-03 22:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+function superbowlWin(arr){
+  arr.find(s => s.result === "W" )
 }
